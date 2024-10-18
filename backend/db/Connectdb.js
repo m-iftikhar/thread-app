9509759a7b21d32cfc209db4435528b04af676ec
m-iftikhar <iftikharrj2001@gmail.com>
2024-10-18 14:42:03 +0500

    remove parser etc

--- backend/db/Connectdb.js
+++ backend/db/Connectdb.js
@@ -1,17 +1,15 @@
-import mongoose from "mongoose";
-const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI, {
-			// To avoid warnings in the console
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
+import mongoose from 'mongoose';
 
-		console.log(`MongoDB Connected: ${conn.connection.host}`);
-	} catch (error) {
-		console.error(`Error: ${error.message}`);
-		process.exit(1);
-	}
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      // No need for useNewUrlParser or useUnifiedTopology in MongoDB 4.0.0+
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
