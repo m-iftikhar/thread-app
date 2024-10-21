246487ef28bc03afbdebbce71f55cdaa157c5bf7
m-iftikhar <iftikharrj2001@gmail.com>
2024-10-21 22:45:13 +0500

    add getuser post route

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { createPost,deletePost,getPost,likeUnlikePost,replyToPost,getFeedPosts } from "../controllers/postController.js";
+import { createPost,deletePost,getPost,likeUnlikePost,replyToPost,getFeedPosts,getUserPosts } from "../controllers/postController.js";
 import protectRoute from "../middleware/protecRoute.js"
 
 
@@ -8,6 +8,7 @@ import protectRoute from "../middleware/protecRoute.js"
 router.post("/create", protectRoute, createPost);
 router.get("/feed", protectRoute, getFeedPosts);
 router.get("/:id", getPost);
+router.get("/user/:username", getUserPosts);
 router.delete("/:id",protectRoute, deletePost);
 router.put("/like/:id", protectRoute, likeUnlikePost);
 router.put("/reply/:id", protectRoute, replyToPost);
