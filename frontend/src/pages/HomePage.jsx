7d8c0fee2a40a3e7fec0b69959461f71c2cf7e06
m-iftikhar <iftikharrj2001@gmail.com>
2024-10-23 00:37:43 +0500

    add post atomhook init

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,32 +2,34 @@ import { Flex, Spinner, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import useShowToast from "../../hooks/useShowToast";
 import Post from "../components/Posts.jsx";
-
+import postsAtom from "../../atom/postAtom.js";
+import { useRecoilState } from "recoil";
 const HomePage = () => {
   const [loading, setLoading] = useState(true);
-  const [posts, setPosts] = useState([]); // Initialize posts state
+  const [posts, setPosts] = useRecoilState(postsAtom);
   const showToast = useShowToast();
 
   useEffect(() => {
-    const getFeedPosts = async () => {
-      setLoading(true);
-      setPosts([]);
-      try {
-        const res = await fetch("/api/posts/feed");
-        const data = await res.json();
-        if (data.error) {
-          showToast("Error", data.error, "error");
-          return;
-        }
-        setPosts(data); // Set fetched posts
-      } catch (error) {
-        showToast("Error", error.message, "error");
-      } finally {
-        setLoading(false);
-      }
-    };
-    getFeedPosts();
-  }, [showToast]);
+		const getFeedPosts = async () => {
+			setLoading(true);
+			setPosts([]);
+			try {
+				const res = await fetch("/api/posts/feed");
+				const data = await res.json();
+				if (data.error) {
+					showToast("Error", data.error, "error");
+					return;
+				}
+				console.log(data);
+				setPosts(data);
+			} catch (error) {
+				showToast("Error", error.message, "error");
+			} finally {
+				setLoading(false);
+			}
+		};
+		getFeedPosts();
+	}, [showToast, setPosts]);
 
   return (
     <>
