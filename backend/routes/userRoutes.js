4cb8b96cfccb08a496fbeebf52516b3a28f3b24e
m-iftikhar <iftikharrj2001@gmail.com>
2024-11-05 00:04:57 +0500

    suggested user done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
-import {signupUser,loginUser, logoutUser,followUnFollowUser,updateUser, getUserProfile} from "../controllers/userController.js";
+import {signupUser,loginUser, logoutUser,followUnFollowUser,updateUser, getUserProfile,getSuggestedUsers} from "../controllers/userController.js";
 import protectRoute from "../middleware/protecRoute.js";
 // Define signup route
 router.post("/signup", signupUser);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 router.post("/follow/:id",protectRoute, followUnFollowUser);
